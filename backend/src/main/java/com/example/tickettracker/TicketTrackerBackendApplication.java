c7517e8cd3711070628a539e18983896690d4f4c
Ariglos <piotr4165@wp.pl>
2024-09-01 18:39:55 +0200

    init backend

---
+++ backend/src/main/java/com/example/tickettracker/TicketTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tickettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/tickettracker/TicketTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tickettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
